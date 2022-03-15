7d07a9973192e974cf5dd375fa7285dfcc68e46b
deepak-singhh <deepakshivam1998@gmail.com>
2022-03-15 20:07:38 +0530

    15-march-2022

---
+++ 15-march-2022/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+  
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+    
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+    
+app.post('/', urlencodedParser, (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+    
+app.listen(3000,()=>{
+    console.log("SERVER STARTED ON PORT:",3000)
+});
